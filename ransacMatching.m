f60072debfc3b20da91c9ef457f4dd0ab9e93dd6
dexterr91 <a.zugarini91@gmail.com>
2014-08-17 13:45:38 +0200

    RGB D aligned

--- ransacMatching.m
+++ ransacMatching.m
@@ -1,9 +1,9 @@
-function [H, X1, X2] = ransacMatching(keypoints1, descriptors1, keypoints2, descriptors2)
+function [H, X1, X2,matches] = ransacMatching(keypoints1, descriptors1, keypoints2, descriptors2)
 
 [matches, scores] = vl_ubcmatch(descriptors1, descriptors2);
-
 X1 = keypoints1(1:2,matches(1,:)) ; 
-X2 = keypoints2(1:2,matches(2,:)) ; 
+X2 = keypoints2(1:2,matches(2,:)) ;
+%keypoints2(:,matches(2,:)) = []
 
 t = cp2tform(X1', X2', 'affine');
 X1(3,:) = 1 ;
